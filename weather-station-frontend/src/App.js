74ad9ca2821d17db9562f2b2c958d3a94463f275
Filip <fifikrawczak@interia.pl>
2024-10-17 18:46:57 +0200

    - add navigation menu

--- weather-station-frontend/src/App.js
+++ weather-station-frontend/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SensorList from './components/SensorList';
 import SensorDetails from './components/SensorDetails';
 import TokenBalance from './components/TokenBalance';
+import NavMenu from './components/NavMenu';
 
 function App() {
   return (
       <Router>
-        <Routes>
-            <Route path="/" element={<SensorList />} />
-            <Route path="/sensors/:id" element={<SensorDetails />} />
-            <Route path="/tokens" element={<TokenBalance />} />
-        </Routes>
+        <div>
+            <NavMenu/>
+            <Routes>
+                <Route path="/" element={<SensorList />} />
+                <Route path="/sensors/:id" element={<SensorDetails />} />
+                <Route path="/tokens" element={<TokenBalance />} />
+            </Routes>
+        </div>
       </Router>
   );
 }
