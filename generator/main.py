475318b4ae50cad4b164bc028434154d439b8442
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2024-10-20 16:03:09 +0200

    Refactor generator

--- generator/main.py
+++ generator/main.py
@@ -1,45 +1,23 @@
 import time
-from sensor import Sensor
+from sensor import Sensor, get_all_sensors, create_sensors, delete_all_sensors
 from concurrent.futures import ThreadPoolExecutor, Future
 import argparse
 
-SENSORS_FOR_EACH_TYPE = 4
 ALL_SENSORS = 16
-ADDRESSES = [
-    "0xB59aB3f970befDd1e1B009376083FB69c339D5ba",
-    "0x16f870C925Fe2399786d318B627E9c159e13FD38",
-    "0x5E5Ba4Ae271De64E839421701Ec4420D32CCAf0D",
-    "0xfa48f36BA6Cc563eEf20eCA4eDBfc92d29b521cB",
-    "0xaD04B6f85210F7BAc634972e32fa0E99C487B1f0",
-    "0xc6B43723dC57E444Ee3144E5Db34453bA4b7fe58",
-    "0xD65bb6b3B2B492feAf6d176721b5950B39b2EF5d",
-    "0xCed537Ef904039c5f1218069b10992D5727B4042",
-    "0xDEE8acEA1E0475cbA90300DabDbf9393dD200Ab6",
-    "0xBADC41713A33E692c3A8f0688184d42c314AE20f",
-    "0x6ffB0279E0C65990eE1aF6CeBA6903296fa960a7",
-    "0x9108f1907295Aec304FE9d23B4f022BE9e509928",
-    "0xBD213a69d31F225Bf20442782634ed74F4655B7a",
-    "0x8c2683e6a9dE0B84cA9AEE43C6a0F8fa40c99113",
-    "0x16ACba43Ff56D02E2c583A6803d21045068677DE",
-    "0xDB54b838Fe344527AF40791bbDbea02cce1e0978"
-]
 
-def create_sensors():
-    print("Creating sensors...")
-    iterator = 0
-    for sensor_type in Sensor.SENSOR_INFO.keys():
-        for i in range(SENSORS_FOR_EACH_TYPE):
-            sensor_name = f"{sensor_type[:4].lower()}#{i+1}"
-            Sensor.create(sensor_name, sensor_type, ADDRESSES[iterator])
-            iterator += 1
 
 def start_simulation():
-    all_sensors = Sensor.get_all_sensors()
-    sensors_info = [Sensor(sensor_data["id"], sensor_data["type"], sensor_data["name"]) for sensor_data in all_sensors]
+    all_sensors = get_all_sensors()
+    sensors_info = [
+        Sensor(sensor_data["id"], sensor_data["type"], sensor_data["name"])
+        for sensor_data in all_sensors
+    ]
 
     print("Starting simulation...")
     with ThreadPoolExecutor(max_workers=ALL_SENSORS) as executor:
-        futures: dict[Future, Sensor] = {executor.submit(sensor.send_data): sensor for sensor in sensors_info}
+        futures: dict[Future, Sensor] = {
+            executor.submit(sensor.send_data): sensor for sensor in sensors_info
+        }
 
         try:
             while not Sensor.STOP_SIMULATION.is_set():
@@ -50,21 +28,40 @@ def start_simulation():
 
     print("Simulation stopped. All threads finished.")
 
+
 def send_single_value(sensor_id: str, value: float):
-    all_sensors = Sensor.get_all_sensors()
-    sensor_data = next((sensor for sensor in all_sensors if sensor["id"] == sensor_id), None)
+    all_sensors = get_all_sensors()
+    sensor_data = next(
+        (sensor for sensor in all_sensors if sensor["id"] == sensor_id), None
+    )
     if sensor_data:
         sensor = Sensor(sensor_data["id"], sensor_data["type"], sensor_data["name"])
         sensor.send_data(value)
     else:
         print(f"Sensor {sensor_id} not found.")
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Sensor data simulation and management.")
-    parser.add_argument('--single', metavar='id:value', type=str, help="send a single value by a sensor (format: sensor_id:value)", default=None)
-    parser.add_argument('--create', action='store_true', help="only create sensors and exit", default=False)
-    parser.add_argument('--start', action='store_true', help="only start the simulation", default=False)
-    
+    parser = argparse.ArgumentParser(
+        description="Sensor data simulation and management."
+    )
+    parser.add_argument(
+        "--single",
+        metavar="id:value",
+        type=str,
+        help="send a single value by a sensor (format: sensor_id:value)",
+        default=None,
+    )
+    parser.add_argument(
+        "--create", action="store_true", help="create sensors", default=False
+    )
+    parser.add_argument(
+        "--start", action="store_true", help="start the simulation", default=False
+    )
+    parser.add_argument(
+        "--delete", action="store_true", help="delete all sensors", default=False
+    )
+
     args = parser.parse_args()
 
     if args.single:
@@ -74,13 +71,13 @@ def main():
         create_sensors()
     elif args.start:
         start_simulation()
+    elif args.delete:
+        delete_all_sensors()
     else:
-        Sensor.delete_all_sensors()
-        if input("Are you ready to start the simulation? (y/n): ").lower() != 'y':
-            print("Exiting...")
-            return
+        delete_all_sensors()
         create_sensors()
         start_simulation()
+
 
 if __name__ == "__main__":
     main()
